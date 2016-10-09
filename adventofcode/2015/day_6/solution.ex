f242ef8014ec28e671b4ff6bd170c87d9f59fb0c
monstercoder <monstercoder@gmail.com>
2016-10-09 12:13:39 -0500

    swith to use sets instead of list

--- adventofcode/2015/day_6/solution.ex
+++ adventofcode/2015/day_6/solution.ex
@@ -4,39 +4,36 @@ defmodule Solution do
     |> File.stream!
     |> Stream.map(&String.strip/1)
     |> Stream.map(&(String.split(&1," ")))
-    |> Enum.map(&debug/1)
-    |> Enum.reduce([], &parse_instruction/2)
+    |> Enum.reduce(MapSet.new, &parse_instruction/2)
     |> Enum.count
     |> inspect
     |> IO.puts
   end
 
-  def debug(x) do
-    IO.puts x
-    x
-  end
   def parse_instruction(["turn","on",from,"through", to], lights_on)  do
-    grid(point(from), point(to)) ++ lights_on
-    |> Enum.uniq
+    grid(point(from), point(to))
+    |> MapSet.union(lights_on)
   end
 
   def parse_instruction(["turn","off",from,"through",to], lights_on) do
-    lights_on -- grid(point(from), point(to))
+     grid(point(from), point(to))
+     |> Enum.reduce(lights_on, &(MapSet.delete(&2, &1)))
   end
 
   def parse_instruction(["toggle", from, "through", to], lights_on) do
-    lights_on ++ grid(point(from), point(to))
-    |> Enum.sort(fn ({x,y},{k,v}) -> (y - v) +(x-k) * 10000 <0 end )
-    |> remove_double_element
+    to_toggles =grid(point(from), point(to))
+    to_turn_off = MapSet.intersection(lights_on, to_toggles)
+    to_turn_on =  MapSet.difference(to_toggles, lights_on)
+
+    to_turn_off
+    |> Enum.reduce(lights_on, &(MapSet.delete(&2, &1)))
+    |> MapSet.union(to_turn_on)  
   end
 
-  def remove_double_element([]), do: []
-  def remove_double_element([x]), do: [x]
-  def remove_double_element([x,y|tail]) when x==y, do: remove_double_element(tail)
-  def remove_double_element([x,y|tail]), do: [x|remove_double_element([y |tail])]
   
   def grid([x,y], [k,v]) do
-    for a <- x..k, b <- y..v, do: {a, b} 
+    (for a <- x..k, b <- y..v, do: {a, b})
+    |> MapSet.new
   end
 
   def point(str) do

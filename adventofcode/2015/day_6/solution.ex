8bdc888b117eb99709e4a8a219c1f3cbb92b39dd
monstercoder <monstercoder@gmail.com>
2016-10-08 22:34:14 -0500

    impl. toogle

--- adventofcode/2015/day_6/solution.ex
+++ adventofcode/2015/day_6/solution.ex
@@ -1,12 +1,24 @@
 defmodule Solution do
   def parse_instruction(["turn","on",from,"through", to], lights_on)  do
     grid(point(from), point(to)) ++ lights_on
+    |> Enum.uniq
   end
 
   def parse_instruction(["turn","off",from,"through",to], lights_on) do
     lights_on -- grid(point(from), point(to))
   end
 
+  def parse_instruction(["toggle", from, "through", to], lights_on) do
+    lights_on ++ grid(point(from), point(to))
+    |> Enum.sort(fn ({x,y},{k,v}) -> (y - v) +(x-k) * 10000 <0 end )
+    |> remove_double_element
+  end
+
+  def remove_double_element([]), do: []
+  def remove_double_element([x]), do: [x]
+  def remove_double_element([x,y|tail]) when x==y, do: remove_double_element(tail)
+  def remove_double_element([x,y|tail]), do: [x|remove_double_element([y |tail])]
+  
   def grid([x,y], [k,v]) do
     for a <- x..k, b <- y..v, do: {a, b} 
   end
--- adventofcode/2015/day_6/solution_test.ex
+++ adventofcode/2015/day_6/solution_test.ex
@@ -4,13 +4,16 @@ defmodule Solution_test do
   use ExUnit.Case, async: true
   import Solution, only: [parse_instruction: 2]
   test "turn on 0,0 to 1,2" do
-    assert parse_instruction(["turn","on","0,0","through","1,2"], [{4,4}]) == [{0,0}, {0,1}, {0,2}, {1,0}, {1,1},{1,2}, {4,4}]
+    assert parse_instruction(["turn","on","0,0","through","1,2"], [{0,0}, {4,4}]) == [{0,0}, {0,1}, {0,2}, {1,0}, {1,1},{1,2}, {4,4}]
   end
   
   test "turn off  0,0 to 1,2" do
     assert parse_instruction(["turn","off","0,0","through","1,2"], [{0,0},{0,2},{4,4}]) == [{4,4}]
   end
   
+  test "toggle  1,1 to 1,2" do
+    assert parse_instruction(["toggle","1,1","through","1,2"], [{0,0},{1,2},{4,4}]) == [{0,0},{1,1},{4,4}]
+  end
 
   
 end

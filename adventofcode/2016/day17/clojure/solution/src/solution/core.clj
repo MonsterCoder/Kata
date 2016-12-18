d7a4eb0caf75d93b1c555620060cc10bec028bbd
monstercoder <monstercoder@gmail.com>
2016-12-18 15:05:57 -0600

    day17 part 1

--- adventofcode/2016/day17/clojure/solution/src/solution/core.clj
+++ adventofcode/2016/day17/clojure/solution/src/solution/core.clj
@@ -1,5 +1,6 @@
- solution.core
+ (ns solution.core
   (:import java.security.MessageDigest))
+(declare step shortest)
 (def moves {0 [0 -1] 1 [0 1] 2 [-1 0] 3 [1 0]}) 
 (defn md5 [s]
     (->> (-> (MessageDigest/getInstance "md5")
@@ -14,32 +15,26 @@
 
 (defn valid? [[c [x y] path]]
   (and (>= x 0) (>= y 0) (< x 4) (< y 4) (.contains (seq "bcdef") c)))
-(defn debug [x]
-  (println "---"  x)
-  ;;(->> (map-indexed #(println (str %1 " " %2 " " pt  " " passcode)) passcode)
-  (identity x))
 
+(defn shortest [[l p] [c pt passcode]]
+ (if (>= (.length passcode ) l) 
+      (identity [l p])
+      (let ([[x y] (step pt passcode [l p])] (if (< x l) (identity [x y]) (identity [l p]))))))
+
+(defn shortest2 [[l p] [c pt passcode]]
+  ;;(println l p c pt passcode)
+  (if (> l (.length passcode))
+    (let [[x y](step pt passcode [l p])] (if (> l x) (identity [x y]) (identity [l p])))
+    (identity [l p])))
 (defn step 
-  ([pt passcode]
-    (let [[x y] pt] (println (str "*" x "-" y "*" passcode)))
+  ([pt passcode] (step pt passcode [99999999 passcode]))
+  ([pt passcode best]
+    ;;(let [[x y] pt] (println (str "*" x "-" y "*" best " * "  passcode)))
     (if (= pt [3 3])
-      (.length passcode)
+      (identity [(.length passcode) passcode])
       ( ->> (md5-hash passcode)
         (map-indexed #(mv %1 %2 pt passcode ))
         (filter valid?)
-        (#(if (empty? %) 99999999 (apply min (map step %))))
-      )))
-  ([[c pt passcode]] (step pt passcode)))
+        (#(if (empty? %) [99999999 passcode] (reduce shortest2 best %)))
+      ))))
 
-
-(defn tt [x]
-  (let [[c pt pc] x] (step pt pc)))
-
-(defn run [pt passcode]
-  (->> (step pt passcode)
-       (first)
-       (step)
-       (first)
-       (step)
-       (first)
-       (#(if (nil? %) 99999 (step %)))))
--- adventofcode/2016/day17/clojure/solution/test/solution/core_test.clj
+++ adventofcode/2016/day17/clojure/solution/test/solution/core_test.clj
@@ -5,3 +5,11 @@
 (deftest hash-test
   (testing "hijkl"
     (is (= (md5-hash "hijkl") "ced9"))))
+
+(deftest path-test
+  (testing "ihgpwlah"
+    (is (= (second (step [0 0]  "ihgpwlah")) "ihgpwlahDDRRRD")))
+  (testing "kglvqrro"
+    (is (= (second (step [0 0]  "kglvqrro")) "kglvqrroDDUDRLRRUDRD")))
+  (testing "ulqzkmiv"
+    (is (= (second (step [0 0]  "ulqzkmiv")) "ulqzkmivDRURDRUDDLLDLUURRDULRLDUUDDDRR"))))

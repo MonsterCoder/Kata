b563854459dfa894b004d2d5bcef2ab6dd83a8a5
monstercoder <monstercoder@gmail.com>
2016-12-18 12:41:16 -0600

    day17

--- adventofcode/2016/day17/clojure/solution/src/solution/core.clj
+++ adventofcode/2016/day17/clojure/solution/src/solution/core.clj
@@ -10,13 +10,36 @@
   (subs (md5 s) 0 4))
 
 (defn mv [pos c pt pc]
-  [c (map + (get  moves pos) pt)  (str pc c) ])
+  [c (into [] (map + (get  moves pos) pt))  (str pc (get ["U" "D" "L" "R"] pos)) ])
 
 (defn valid? [[c [x y] path]]
-  (and (>= x 0) (>= y 0) (.contains (seq "bcdef") c)))
-  
-(defn step [passcode pt]
- ( ->> (md5-hash passcode)
-    (map-indexed #(mv %1 %2 pt passcode ))
-    (filter valid?)))
+  (and (>= x 0) (>= y 0) (< x 4) (< y 4) (.contains (seq "bcdef") c)))
+(defn debug [x]
+  (println "---"  x)
+  ;;(->> (map-indexed #(println (str %1 " " %2 " " pt  " " passcode)) passcode)
+  (identity x))
 
+(defn step 
+  ([pt passcode]
+    (let [[x y] pt] (println (str "*" x "-" y "*" passcode)))
+    (if (= pt [3 3])
+      (.length passcode)
+      ( ->> (md5-hash passcode)
+        (map-indexed #(mv %1 %2 pt passcode ))
+        (filter valid?)
+        (#(if (empty? %) 99999999 (apply min (map step %))))
+      )))
+  ([[c pt passcode]] (step pt passcode)))
+
+
+(defn tt [x]
+  (let [[c pt pc] x] (step pt pc)))
+
+(defn run [pt passcode]
+  (->> (step pt passcode)
+       (first)
+       (step)
+       (first)
+       (step)
+       (first)
+       (#(if (nil? %) 99999 (step %)))))

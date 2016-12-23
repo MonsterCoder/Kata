515bb7716164f84908be5757a86b089fa0db1fcb
monstercoder <monstercoder@gmail.com>
2016-12-23 13:58:22 -0600

    adventofcode 2016 day19 part 2

--- adventofcode/2016/day19/solution/src/solution/core.clj
+++ adventofcode/2016/day19/solution/src/solution/core.clj
@@ -10,10 +10,23 @@
   (if (nil? cc)
     (recur next [])
     (if (= 1 (count cc))
-      (if (empty? next)
+      (if (empty? )
         cc
         (recur (concat cc next) []))
       (let [[[p1 n1] [p2 n2] & rest] cc] 
         (recur rest (conj next [p1 (+ n1 n2)]))))))
     
-    
+(defn pick [lst]
+  "pick the middle left"
+  (let [p (int (Math/ceil (/ (count lst) 2)))]
+    (let [[f s] (split-at p lst)]
+      [(last f) (concat (drop-last f) s)]))  
+)
+(defn play2 [cc]
+  "play the game in rule2"
+  ;;(println cc)
+  (if (= 1 (count cc))
+    cc
+    (let [[[p n] & rest] cc]
+      (let [[[p2 n2] next] (pick rest)]
+         (recur  (concat next [[p (+ n n2)]] ))))))
